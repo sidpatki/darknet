52a0e7e1c6cd1cee799d992fa33504cfcf787a53
Siddharth Patki <spatki@ur.rochester.edu>
2019-04-26 13:15:48 -0400

    minor

--- train.sh
+++ train.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Ball/Ball.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Computer_keyboard/Computer_keyboard.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Computer_monitor/Computer_monitor.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Computer_mouse/Computer_mouse.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Drawer/Drawer.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Headphones/Headphones.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Helmet/Helmet.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Microwave_oven/Microwave_oven.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Shelf/Shelf.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Fire_hydrant/Fire_hydrant.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/Fire_hydrant/yolo_tiny_plus_single.backup
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Car/Car.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Tree/Tree.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Bench/Bench.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Chair/Chair.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Screwdriver/Screwdriver.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Hammer/Hammer.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Drill/Drill.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Person/Person.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/Person/yolo_tiny_plus_single.backup
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Table/Table.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74
+# ./darknet detector train ../OIDv4_ToolKit/OID/Dataset/train/Drawer/Drawer.data ./cfg/yolo_tiny_plus_single.cfg ../trained_weights/pretrained_imagenet/darknet53.conv.74 // didnt work for some reason
